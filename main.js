8b2c009c12655eaf67b27d209e8d14b50db65bac
Vitaly <vitalyb@gmail.com>
2018-02-25 01:36:16 +0200

    cors support

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 const _ = require("lodash");
 const url = require("url");
-const mkdirRecursive = require('mkdir-recursive');
+const mkdirRecursive = require("mkdir-recursive");
 const path = require("path");
-const endOfLine = require('os').EOL;
+const endOfLine = require("os").EOL;
 
 const content = fs.readFileSync("har/test1.har", "utf8");
 //const content = fs.readFileSync("har/test2.har", "utf8");
-
 
 const har = JSON.parse(content);
 const entries = har.log.entries;
@@ -40,18 +39,22 @@ filteredEntries = filterOutUrl(filteredEntries, /\.html$/);
 
 _.sortBy(filteredEntries, entry => entry.request.url).forEach(entry => {
   const entryUrl = entry.request.url;
-  console.log("Processing: " + entryUrl)
+  console.log("Processing: " + entryUrl);
   const parsedUrl = url.parse(entryUrl);
   const directory = path.join("mockserver", parsedUrl.pathname);
 
   const method = entry.request.method;
 
-  const headerStatus = "HTTP/1.1 " + entry.response.status;
-  const headerContentType = "Content-Type: " + getContentTypeHeader(entry).value;
+  const headers = [
+    "HTTP/1.1 " + entry.response.status,
+    "Content-Type: " + getContentTypeHeader(entry).value,
+    "Access-Control-Allow-Origin: *",
+    "Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS"
+  ];
   const content = entry.response.content.text;
-  const fileContent = headerStatus + endOfLine + headerContentType + endOfLine + endOfLine + content;
+
+  const fileContent = headers.join(endOfLine) + endOfLine + endOfLine + content;
 
   mkdirRecursive.mkdirSync(directory);
   fs.writeFileSync(path.join(directory, method + ".mock"), fileContent);
-
 });
